e26e8dd07931cae4da69fec84cf3fb7492131e9a
gatosimple <20222730@aloe.ulima.edu.pe>
2025-11-10 20:56:10 -0500

    Login arreglado

--- src/modules/iniciar-sesion/use-cases/LoginUseCase.ts
+++ src/modules/iniciar-sesion/use-cases/LoginUseCase.ts
@@ -1,5 +1,6 @@
 import { UsuarioRepository } from '../../../infrastructure/repositories/UsuarioRepository'
-import { LoginParamsDto, LoginResultDto } from '../dtos/LoginDto'
+import { LoginParamsDto } from '../dtos/LoginParamsDto'
+import { LoginResultDto } from '../dtos/LoginResultDto'
 
 export class LoginUseCase {
   constructor(private usuarioRepository: UsuarioRepository) {}
@@ -31,8 +32,9 @@ export class LoginUseCase {
         usuario_id: user.usuario_id,
         correo: user.correo,
         nombre: user.cliente?.nombre ?? null,
-        apellido: user.cliente?.apellido ?? null
-      },
+        apellido: user.cliente?.apellido ?? null,
+        foto_perfil: user.cliente?.foto_perfil ?? null
+      }
     }
     return result
   }
